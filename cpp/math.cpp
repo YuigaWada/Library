e821509d87a615bc270d8cd58031aeed9c28bf5d
YuigaWada <yuigawada@gmail.com>
2021-01-27 18:06:21 +0900

    Update math.cpp

--- cpp/math.cpp
+++ cpp/math.cpp
@@ -519,3 +519,90 @@ ll modinv(ll a, ll mod) {
 }
  
 // #PORT_END#
+
+// #PORT#
+// name: "modint_noconst"
+// prefix: "modint_noconst"
+// description: "可変modint"
+
+ll MOD;
+
+struct ModInt {
+    ll x;
+    ll mod = MOD;
+
+    ModInt() : x(0) {}
+
+    ModInt(ll y) {
+        x = y >= 0 ? y % mod : (mod - (-y) % mod) % mod;
+    }
+
+    ModInt &operator+=(const ModInt &p) {
+        if ((x += p.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    ModInt &operator-=(const ModInt &p) {
+        if ((x += mod - p.x) >= mod) x -= mod;
+        return *this;
+    }
+
+    ModInt &operator*=(const ModInt &p) {
+        x = (int)(1LL * x * p.x % mod);
+        return *this;
+    }
+
+    ModInt &operator/=(const ModInt &p) {
+        *this *= p.inverse();
+        return *this;
+    }
+
+    ModInt operator-() const { return ModInt(-x); }
+
+    ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+
+    ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+
+    ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+
+    ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+
+    bool operator==(const ModInt &p) const { return x == p.x; }
+
+    bool operator!=(const ModInt &p) const { return x != p.x; }
+
+    ModInt inverse() const {
+        int a = x, b = mod, u = 1, v = 0, t;
+        while (b > 0) {
+            t = a / b;
+            swap(a -= t * b, b);
+            swap(u -= t * v, v);
+        }
+        return ModInt(u);
+    }
+
+    ModInt pow(ll n) const {
+        ModInt ret(1), mul(x);
+        while (n > 0) {
+            if (n & 1) ret *= mul;
+            mul *= mul;
+            n >>= 1;
+        }
+        return ret;
+    }
+
+    friend ostream &operator<<(ostream &os, const ModInt &p) {
+        return os << p.x;
+    }
+
+    friend istream &operator>>(istream &is, ModInt &a) {
+        ll t;
+        is >> t;
+        a = ModInt(t);
+        return (is);
+    }
+};
+
+using modint = ModInt;
+
+// #PORT_END#
